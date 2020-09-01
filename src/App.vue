b66988b1bb90d371e7d894f6c136bc5fd07bd117
Jarrett <1206905682@qq.com>
2020-09-01 18:38:43 +0800

    Topnav封装使用

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <template>
-<div>导航栏 |
-    <router-link to="/">Frank</router-link>|
-    <router-link to="xxx">Frank2</router-link>
-
-    <hr>
-    <!-- 组件内容展示-->
-    <router-view />
+<!-- 组件内容展示-->
+<router-view />
 </template>
 
 <script>
 export default {
-    name: 'App'
-}
+    name: "App",
+};
 </script>
--- src/components/Frank.vue
+++
@@ -1,3 +0,0 @@
-<template>
-这是1
-</template>
