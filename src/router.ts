07d6c189b4df79a0607c40df4ecec70d7a3b9615
Jarrett <1206905682@qq.com>
2020-09-02 13:08:46 +0800

    基本结构完成

--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,28 @@
-import {createWebHashHistory,createRouter} from 'vue-router'
+import { createWebHashHistory, createRouter } from 'vue-router'
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
 import Switch from './components/Switch.vue'
+import Button from './components/Button.vue'
+import Dialog from './components/Dialog.vue'
+import Tabs from './components/Tabs.vue'
+import DocDefault from './components/DocDefault.vue'
 
-const history=createWebHashHistory()
- export const router=createRouter(
+const history = createWebHashHistory()
+export const router = createRouter(
     {
-        history:history,
-        routes:[
-            {path:'/',component:Home},
-            {path:'/doc',component:Doc,children:[
-                {path:'switch',component:Switch}
-            ]}
+        history: history,
+        routes: [
+            { path: '/', component: Home },
+            {
+                path: '/doc', component: Doc, children: [
+                    {path:'/',component:DocDefault},
+                    { path: 'switch', component: Switch },
+                    { path: 'switch', component: Button },
+                    { path: 'switch', component: Dialog },
+                    { path: 'switch', component: Tabs }
+
+                ]
+            }
         ]
     }
 )
