bb73308beb682be0836c34e81bf252b4d259892a
Jarrett <1206905682@qq.com>
2020-09-04 15:27:27 +0800

    创建button组件

---
+++ src/components/ButtonDemo.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    Button示例
+</div>
+<Button @click="onClick" @focus="onClick" @mouseover="onClick" size="small" level="normal" disabled>你好</Button>
+</template>
+
+<script lang="ts">
+import Button from '../lib/Button.vue'
+export default {
+    components: {
+        Button
+    },
+    setup() {
+        const onClick = () => {
+            console.log('hi')
+        }
+    }
+}
+</script>
---
+++ src/lib/Button.vue
@@ -0,0 +1,44 @@
+<template>
+<div :size="size">
+    <button v-bind="rest">
+        <!-- <button v-bind="$attrs">-->
+        <!--插槽传值 -->
+        <slot />
+    </button>
+</div>
+</template>
+
+<script lang="ts">
+/*
+默认所有属性绑定到根元素
+可以使用inheritAttrs:false取消默认绑定
+使用$attrs或者context.attrs获取所有属性
+使用v-bind="$attrs"可以批量绑定属性
+使用const{size,..rest}=context.attrs将拿到的属性分开，用到了es6剩余操作符
+*/
+/*
+attr和props区别：
+props要先声明props:{
+    size:String
+}
+而attrs不用先声明
+props不包含事件，attrs包含
+在props中没有声明的属性，会跑到attrs里
+props支持string以外的类型，attrs只有string
+props需要
+*/
+export default {
+    // 防止定义在button外层元素
+    inheritAttrs: false,
+    setup(props, context) {
+        const {
+            size,
+            ...rest
+        } = context.attrs;
+        return {
+            size,
+            rest,
+        };
+    },
+};
+</script>
