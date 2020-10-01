b2af11b1ca4b1afcbe49cc54b066ec66068947bb
Jarrett <1206905682@qq.com>
2020-10-01 17:21:45 +0800

    完成dialog组件展示

---
+++ src/components/demos/dialog/DialogDemo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>Dialog示例</div>
+  <Demo :component="DialogDemo1"></Demo>
+  <Demo :component="DialogDemo2"></Demo>
+
+</template>
+
+<script lang="ts">
+import Demo from "../../Demo.vue";
+import DialogDemo1 from "./DialogDemo1.vue";
+import DialogDemo2 from "./DialogDemo2.vue";
+
+export default {
+  components: {Demo},
+  setup() {
+    return {
+      DialogDemo1,
+      DialogDemo2,
+    };
+  }
+};
+</script>
